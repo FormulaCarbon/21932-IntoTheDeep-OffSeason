0bb330ec29808ba35df20808351e7438d5f42768
FormulaCarbon <99456163+FormulaCarbon@users.noreply.github.com>
2025-05-11 13:00:51 -0500

    fbgfyfjft

--- TeamCode/src/main/java/pedroPathing/tuners_tests/localization/LocalizationTest.java
+++ TeamCode/src/main/java/pedroPathing/tuners_tests/localization/LocalizationTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.pedroPathing.tuners_tests.localization;
+package pedroPathing.tuners_tests.localization;
 
 import static com.pedropathing.follower.FollowerConstants.leftFrontMotorName;
 import static com.pedropathing.follower.FollowerConstants.leftRearMotorName;
@@ -27,8 +27,7 @@ import com.pedropathing.util.Drawing;
 import java.util.Arrays;
 import java.util.List;
 
-import org.firstinspires.ftc.teamcode.pedroPathing.constants.FConstants;
-import org.firstinspires.ftc.teamcode.pedroPathing.constants.LConstants;
+import pedroPathing.constants.*;
 
 /**
  * This is the LocalizationTest OpMode. This is basically just a simple mecanum drive attached to a
